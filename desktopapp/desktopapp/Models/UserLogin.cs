e597d4f854f14c554da9c04b701818f7e49f087b
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-03-17 22:00:23 +0100

    UserLogin and App xaml

---
+++ desktopapp/desktopapp/Models/UserLogin.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace desktopapp.Models
+{
+    public class UserLogin
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
