381e19d4fac07785d8ba24b7022833ad825b7e7d
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-03-17 22:28:29 +0100

    Added AddTaskWindow xaml & Model

---
+++ desktopapp/desktopapp/Views/AddTaskWindow.xaml.cs
@@ -0,0 +1,16 @@
+﻿using desktopapp.ViewModels;
+using System.Windows;
+
+namespace desktopapp.Views
+{
+    public partial class AddTaskWindow : Window
+    {
+        public AddTaskWindow(AddTaskViewModel viewModel)
+        {
+            InitializeComponent();
+            DataContext = viewModel;
+
+            viewModel.CloseAction = new System.Action(this.Close);
+        }
+    }
+}
