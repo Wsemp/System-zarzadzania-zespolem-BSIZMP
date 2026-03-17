2ae00a2928b5eb571c6bc1919bef4b587c425f85
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-03-17 22:28:45 +0100

    Added TaskModel

---
+++ desktopapp/desktopapp/Models/TaskModel.cs
@@ -0,0 +1,11 @@
+﻿namespace desktopapp.Models
+{
+    public class TaskModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Status { get; set; } 
+        public string AssignedUser { get; set; }
+    }
+}
---
+++ desktopapp/desktopapp/ViewModels/MainViewModel.cs
@@ -0,0 +1,42 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using desktopapp.Models;
+using System.Collections.ObjectModel;
+
+namespace desktopapp.ViewModels
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private ObservableCollection<TaskModel> _tasks;
+
+        public MainViewModel()
+        {
+            LoadMockData();
+        }
+
+        private void LoadMockData()
+        {
+            Tasks = new ObservableCollection<TaskModel>
+            {
+                new TaskModel { Id = 1, Title = "Zaprojektować bazę danych", Description = "Baza w MySQL dla Django", Status = "W trakcie", AssignedUser = "Kuba (Backend)" },
+                new TaskModel { Id = 2, Title = "Zrobić API logowania", Description = "Endpoint /api/login", Status = "Do zrobienia", AssignedUser = "Kuba (Backend)" },
+                new TaskModel { Id = 3, Title = "Widok listy zadań WPF", Description = "Tabela w Desktopie", Status = "Zakończone", AssignedUser = "Ja (Desktop)" },
+                new TaskModel { Id = 4, Title = "Makiety w Figmie", Description = "Kolory i przyciski dla mobile", Status = "Do zrobienia", AssignedUser = "Kasia (Mobile)" }
+            };
+        }
+        [RelayCommand]
+        public void OpenAddTaskWindow()
+        {
+            var addTaskVm = new AddTaskViewModel();
+
+            var window = new Views.AddTaskWindow(addTaskVm);
+            window.ShowDialog();
+            if (addTaskVm.CreatedTask != null)
+            {
+                addTaskVm.CreatedTask.Id = Tasks.Count + 1;
+                Tasks.Insert(0, addTaskVm.CreatedTask);
+            }
+        }
+    }
+}
