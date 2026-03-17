ef8449f725c1f1fa266d18671127591ac2b2a969
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-03-17 21:54:37 +0100

    LoginViewModel

---
+++ desktopapp/desktopapp/ViewModels/LoginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Threading.Tasks;
+using System.Windows;
+using desktopapp.Services;
+
+namespace desktopapp.ViewModels
+{
+    public partial class LoginViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _username;
+
+        [ObservableProperty]
+        private string _password;
+
+
+        [RelayCommand]
+        public async Task LoginAsync()
+        {
+
+            bool isSuccess = await ApiService.Instance.LoginAsync(Username, Password);
+
+            if (isSuccess)
+            {
+                var mainWindow = new MainWindow();
+                mainWindow.Show();
+
+                foreach (Window window in Application.Current.Windows)
+                {
+                    if (window is Views.LoginView)
+                    {
+                        window.Close();
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Błędny login, hasło lub brak połączenia z serwerem.", "Błąd logowania");
+            }
+        }
+    }
+}
