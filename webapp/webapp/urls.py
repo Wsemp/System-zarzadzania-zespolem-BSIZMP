7d228dd4de28a19beb4ab893f0b4c725664f88ae
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-03-21 17:51:17 +0100

    main/ path added

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -4,4 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('admin_login.urls')),
+    path('main/', include('main_page.urls')),
 ]
