622a650dc2e56ed19c3e871552aba54eb6c2fa6f
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-03-23 00:09:36 +0100

    Added Projects

---
+++ desktopapp/desktopapp/Models/ProjectModel.cs
@@ -0,0 +1,16 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace desktopapp.Models
+{
+    public partial class ProjectModel : ObservableObject
+    {
+        [ObservableProperty]
+        private int _id;
+
+        [ObservableProperty]
+        private string _name;
+
+        [ObservableProperty]
+        private string _description;
+    }
+}
