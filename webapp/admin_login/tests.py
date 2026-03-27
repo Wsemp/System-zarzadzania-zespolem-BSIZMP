955b6ec1c2e55ab357b1ca54305691feca0dfb45
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-03-27 07:31:30 +0100

    Test login_admin view

--- webapp/admin_login/tests.py
+++ webapp/admin_login/tests.py
@@ -1,3 +1,66 @@
-from django.test import TestCase
+from django.test import TestCase, Client
+from django.contrib.auth.models import User
+from django.urls import reverse
 
-# Create your tests here.
+
+class LoginViewTest(TestCase):
+
+    def setUp(self):
+        self.client = Client()
+        self.url = "/login/"  # albo reverse('login') jeśli masz nazwę URL
+
+        self.admin_user = User.objects.create_user(
+            username="admin",
+            password="admin123",
+            is_staff=True
+        )
+
+        self.normal_user = User.objects.create_user(
+            username="user",
+            password="user123",
+            is_staff=False
+        )
+
+    def test_get_request(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'admin_login/login.html')
+
+    def test_redirect_if_authenticated(self):
+        self.client.login(username="admin", password="admin123")
+        response = self.client.get(self.url)
+        self.assertRedirects(response, "/")
+
+    def test_valid_admin_login(self):
+        response = self.client.post(self.url, {
+            "username": "admin",
+            "password": "admin123"
+        })
+
+        self.assertRedirects(response, "/")
+
+    def test_valid_admin_login_with_next(self):
+        response = self.client.post(self.url + "?next=/", {
+            "username": "admin",
+            "password": "admin123"
+        })
+
+        self.assertRedirects(response, "/")
+
+    def test_non_admin_user(self):
+        response = self.client.post(self.url, {
+            "username": "user",
+            "password": "user123"
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "User is not admin!")
+
+    def test_invalid_credentials(self):
+        response = self.client.post(self.url, {
+            "username": "wrong",
+            "password": "wrong"
+        })
+
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, "Invalid credentials")
