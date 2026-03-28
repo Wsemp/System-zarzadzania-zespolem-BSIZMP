d5c98347a2c58cd0b9efce7a868f891fc96299d0
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-03-28 19:01:19 +0100

    Get method allowed only for common users

--- webapp/users/API/CRUD_users/views.py
+++ webapp/users/API/CRUD_users/views.py
@@ -1,9 +1,15 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
     permission_classes = [IsAuthenticated]
+
+    def get_permissions(self):
+        if self.action in ['list', 'retrieve']:
+            return [IsAuthenticated()]
+        else:
+            return [IsAdminUser()]
