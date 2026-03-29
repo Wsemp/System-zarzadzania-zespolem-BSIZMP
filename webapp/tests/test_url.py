73557b653e6657d8e0f2aeb40af8833c70394751
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-03-29 19:46:30 +0200

    added tests app

---
+++ webapp/tests/test_url.py
@@ -0,0 +1,77 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase, Client
+
+User = get_user_model()
+
+class URLTests(TestCase):
+
+    refresh_token = None
+
+    def setUp(self):
+        self.client = Client()
+        self.user = User.objects.create_user(
+            username="test",
+            password="123"
+        )
+
+    def test_authenticated_urls(self):
+        self.client.login(username="test", password="123")
+
+        urls = [
+            "/",
+            "/users/",
+            "/users/create/",
+        ]
+
+        for url in urls:
+            response = self.client.get(url)
+            self.assertEqual(response.status_code, 200)
+
+
+    def test_get_api_urls(self):
+        self.client.login(username="test", password="123")
+
+        urls = [
+            "/api/users/",
+            "/api/users/1/",
+        ]
+
+        for url in urls:
+            response = self.client.get(url)
+            print(response.status_code, url)
+            self.assertEqual(response.status_code, 200)
+
+    def test_token_flow(self):
+
+        url = "/api/token"
+
+        data = {
+            "username": "test",
+            "password": "123"
+        }
+
+        # get refresh/access token
+
+        response = self.client.post(url, data)
+        self.assertEqual(response.status_code, 200)
+        json_data = response.json()
+        refresh_token = json_data.get("refresh")
+
+        # refresh access token
+
+        refresh_url = "/api/token/refresh"
+
+        refresh_response = self.client.post(refresh_url, {"refresh": refresh_token})
+        self.assertEqual(refresh_response.status_code, 200)
+        new_access = refresh_response.json().get("access")
+
+        print("Refresh:", refresh_token)
+        print("New Access:", new_access)
+
+
+    def test_public_urls(self):
+
+        url = "/login/"
+
+        response = self.client.get(url)
+        self.assertIn(response.status_code, [200, 302])
