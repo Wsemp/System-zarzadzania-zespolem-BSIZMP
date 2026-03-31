30927e4a44b9638bcc9079ebef168cc838ea0e16
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-03-31 19:18:52 +0200

    add path delete user form

--- webapp/users/urls.py
+++ webapp/users/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import show_users, create_user_view
+from .views import show_users, create_user_view, delete_user_view
 
 urlpatterns = [
     path('', show_users, name='users_view'),
     path('create/', create_user_view, name='user_create'),
+    path("delete/<int:user_id>/", delete_user_view, name="delete_user"),
 ]
