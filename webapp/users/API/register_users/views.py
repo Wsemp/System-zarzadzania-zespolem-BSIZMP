31dedaa6164110e9ea2367c4f82df5ecaed89a96
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-04-01 19:32:35 +0200

    added register/login API

---
+++ webapp/users/API/register_users/views.py
@@ -0,0 +1,22 @@
+from rest_framework import generics
+from django.contrib.auth.models import User
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework_simplejwt.tokens import RefreshToken
+from .serializers import RegisterSerializer
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = RegisterSerializer
+
+    def create(self, request, *args, **kwargs):
+        response = super().create(request, *args, **kwargs)
+        user = User.objects.get(username=response.data['username'])
+
+        refresh = RefreshToken.for_user(user)
+
+        return Response({
+            "user": response.data,
+            "access": str(refresh.access_token),
+            "refresh": str(refresh),
+        }, status=status.HTTP_201_CREATED)
