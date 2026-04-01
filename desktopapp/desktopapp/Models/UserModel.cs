42799b1d8108c170c46af5834fce4aa97ed62c5e
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-04-02 00:01:04 +0200

    Added real register

---
+++ desktopapp/desktopapp/Models/UserModel.cs
@@ -0,0 +1,13 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace desktopapp.Models
+{
+    public partial class UserModel : ObservableObject
+    {
+        [ObservableProperty]
+        private string _username;
+
+        [ObservableProperty]
+        private string _email;
+    }
+}
