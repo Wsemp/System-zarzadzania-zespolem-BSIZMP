1d5d85886ca12d78d592719d520ee60134026c74
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-04-04 07:23:19 +0200

    update

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -9,10 +9,7 @@ load_dotenv()
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = os.getenv("SECRET_KEY")
-
-if not SECRET_KEY:
-    raise ValueError("SECRET_KEY is not set!")
+SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
 
 DEBUG = os.getenv("DEBUG") == "True"
 
