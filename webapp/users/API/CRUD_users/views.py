8907bd5000c3bf123d97acfd019d5bffc3eb764a
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-04-22 20:17:35 +0200

    Daniel's issue fix

--- webapp/users/API/CRUD_users/views.py
+++ webapp/users/API/CRUD_users/views.py
@@ -1,7 +1,12 @@
 from django.contrib.auth.models import User
 from rest_framework import viewsets
 from .serializers import UserSerializer
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
+
+
+class IsOwnerOrAdmin(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user.is_staff or obj.user == request.user
 
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
@@ -11,5 +16,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def get_permissions(self):
         if self.action in ['list', 'retrieve']:
             return [IsAuthenticated()]
+        elif self.action in ['update', 'partial_update']:
+            return [IsAuthenticated(), IsOwnerOrAdmin()]
         else:
             return [IsAdminUser()]
