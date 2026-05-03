3a4a6fc8af8d135657d9fbac7889f7bf14fd5a8e
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-03 20:10:41 +0200

    api project fix

--- webapp/projects/API/CRUD_projects/serializers.py
+++ webapp/projects/API/CRUD_projects/serializers.py
@@ -5,6 +5,12 @@ class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     url = serializers.HyperlinkedIdentityField(
         view_name='project-detail'
     )
+
     class Meta:
         model = Project
         fields = ['url', 'id', 'name', 'description']
+        read_only_fields = ['owner']
+
+    def create(self, validated_data):
+        user = self.context['request'].user
+        return Project.objects.create(owner=user, **validated_data)
