938326aac74e666dcfbfb34713f0ba32d9edb411
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-03 07:50:56 +0200

    updated tests

--- webapp/tests/test_api_tasks.py
+++ webapp/tests/test_api_tasks.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 from django.contrib.auth import get_user_model
 from django.utils import timezone
 from tasks.models import Task, Tag
+from projects.models import Project
 from tasks.services.task_services import create_task, delete_task, update_task
 
 User = get_user_model()
@@ -26,6 +27,12 @@ class TestTaskServices(TestCase):
         self.tag2 = Tag.objects.create(name="tag2")
 
     def test_create_task(self):
+
+        project = Project.objects.create(
+            name="Test project",
+            owner=self.user
+        )
+
         task = create_task(
             created_by=self.user,
             title="Test task",
@@ -33,7 +40,8 @@ class TestTaskServices(TestCase):
             status="todo",
             assigned_to=self.assigned_user,
             tags=[self.tag1, self.tag2],
-            due_date=timezone.now()
+            due_date=timezone.now(),
+            project=project
         )
 
         self.assertEqual(Task.objects.count(), 1)
@@ -45,13 +53,20 @@ class TestTaskServices(TestCase):
         self.assertIn(self.tag1, task.tags.all())
 
     def test_delete_task(self):
+
+        project = Project.objects.create(
+            name="Test project",
+            owner=self.user
+        )
+
         task = Task.objects.create(
             created_by=self.user,
             title="Test",
             description="desc",
             status="todo",
             assigned_to=self.assigned_user,
-            due_date=timezone.now()
+            due_date=timezone.now(),
+            project=project
         )
 
         result = delete_task(task_id=task.id)
@@ -60,13 +75,20 @@ class TestTaskServices(TestCase):
         self.assertEqual(Task.objects.count(), 0)
 
     def test_update_task(self):
+
+        project = Project.objects.create(
+            name="Test project",
+            owner=self.user
+        )
+
         task = Task.objects.create(
             created_by=self.user,
             title="Old",
             description="desc",
             status="todo",
             assigned_to=self.assigned_user,
-            due_date=timezone.now()
+            due_date=timezone.now(),
+            project=project,
         )
 
         task.tags.set([self.tag1])
