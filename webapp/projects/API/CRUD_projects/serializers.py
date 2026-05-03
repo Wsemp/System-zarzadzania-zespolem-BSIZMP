2429237a5a589e252e3d125a170263e6dd1a69b5
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-03 19:00:02 +0200

    quick fix

--- webapp/projects/API/CRUD_projects/serializers.py
+++ webapp/projects/API/CRUD_projects/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from ...models import Project
 
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
+    url = serializers.HyperlinkedIdentityField(
+        view_name='project-detail'
+    )
     class Meta:
         model = Project
         fields = ['url', 'id', 'name', 'description']
--- webapp/projects/API/CRUD_projects/urls.py
+++ webapp/projects/API/CRUD_projects/urls.py
@@ -1,7 +1,12 @@
+from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import ProjectViewSet
 
 router = DefaultRouter()
 router.register("", ProjectViewSet)
 
-urlpatterns = router.urls
+
+
+urlpatterns = [
+    path("/", include(router.urls)),
+]
