f45b439c417de8af4d84337221e51e525ae34e6d
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-03 15:30:49 +0200

    user form update

--- webapp/users/forms.py
+++ webapp/users/forms.py
@@ -5,19 +5,38 @@ User = get_user_model()
 
 
 class UserCreateForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput)
+
+    password = forms.CharField(widget=forms.PasswordInput, label="Hasło")
+    password2 = forms.CharField(widget=forms.PasswordInput, label="Powtórz hasło")
 
     class Meta:
         model = User
-        fields = ["username", "email", "password"]
-
+        fields = ["username", "email", "is_staff", "password"]
+        help_texts = {
+            "username": "",
+        }
     def clean_email(self):
         email = self.cleaned_data["email"]
+        if email == "":
+            return email
         if User.objects.filter(email=email).exists():
             raise forms.ValidationError("Email już istnieje")
         return email
 
+    def clean(self):
+        cleaned_data = super().clean()
+        password = cleaned_data.get("password")
+        password2 = cleaned_data.get("password2")
+
+        if password and password2 and password != password2:
+            raise forms.ValidationError("Hasła nie są takie same")
+
+        return cleaned_data
+
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name']
+        fields = ['username', 'email', 'is_staff']
+        help_texts = {
+            "username": "",
+        }
