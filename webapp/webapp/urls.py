68224f62056cd8672e8a25ac5d2cd5b684700442
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-04 07:30:47 +0200

    minor update

--- webapp/webapp/urls.py
+++ webapp/webapp/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('', include('main_page.urls')),
     path('users/', include('users.urls')),
     path('tasks/', include('tasks.urls')),
+    path('projects/', include('projects.urls')),
     path("api/users", include("users.API.CRUD_users.urls")),
     path("api/users/logout/", include("users.API.logout_users.urls")),
     path("api/tasks", include("tasks.API.CRUD_tasks.urls")),
