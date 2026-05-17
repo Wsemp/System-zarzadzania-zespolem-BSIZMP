a5591320917963735970a52fc6a6b079110612d2
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-17 09:17:29 +0200

    update status field in model tasks

--- webapp/tasks/models.py
+++ webapp/tasks/models.py
@@ -18,7 +18,11 @@ class Task(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField(blank=True)
 
-    status = models.CharField(max_length=20, choices=Status.choices)
+    status = models.CharField(
+        max_length=20,
+        choices=Status.choices,
+        default=Status.TODO,
+    )
 
     assigned_to = models.ForeignKey(
         "auth.User",
