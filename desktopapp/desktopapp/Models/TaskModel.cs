2e5dd09d37968236eec45caaedcb230bbee1d7fd
Daniel321W <162799995+Daniel321W@users.noreply.github.com>
2026-05-20 22:16:39 +0200

    Updated errors

--- desktopapp/desktopapp/Models/TaskModel.cs
+++ desktopapp/desktopapp/Models/TaskModel.cs
@@ -1,55 +1,60 @@
 ﻿using Newtonsoft.Json;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace desktopapp.Models
 {
     public class TaskModel
     {
-        [JsonProperty("project_id")]
-        public int? ProjectId { get; set; }
-        
-        [JsonProperty("tag_ids")]
-        public System.Collections.Generic.List<int> TagIds { get; set; } = new System.Collections.Generic.List<int>();
-
+        [Key]
         [JsonProperty("id")]
         public int Id { get; set; }
 
+        [JsonProperty("project_id")]
+        public int? ProjectId { get; set; }
+        
+        [NotMapped]
+        [JsonProperty("tag_ids")]
+        public System.Collections.Generic.List<int> TagIds { get; set; } = new System.Collections.Generic.List<int>();
+
         [JsonProperty("title")]
-        public string Title { get; set; }
+        public string? Title { get; set; }
 
         [JsonProperty("description")]
-        public string Description { get; set; }
+        public string? Description { get; set; }
 
         [JsonProperty("status")]
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         [JsonProperty("assigned_to")]
         public int? AssignedToId { get; set; }
+        
+        [JsonProperty("due_date")]
+        public DateTime? DueDate { get; set; }
 
+        [NotMapped]
         [JsonIgnore]
-        public string AssignedUser { get; set; }
+        public string? AssignedUser { get; set; }
 
+        [NotMapped]
         [JsonIgnore]
-        public string DisplayStatus
+        public string? DisplayStatus
         {
             get
             {
-                return Status switch
-                {
-                    "todo" => "Do zrobienia",
-                    "in_progress" => "W trakcie",
-                    "done" => "Zakończone",
-                    _ => Status
-                };
+                if (string.Equals(Status, "todo", StringComparison.OrdinalIgnoreCase)) return "Do zrobienia";
+                if (string.Equals(Status, "In progress", StringComparison.OrdinalIgnoreCase)) return "W trakcie";
+                if (string.Equals(Status, "done", StringComparison.OrdinalIgnoreCase)) return "Zakończone";
+                
+                return Status; 
             }
             set
             {
-                Status = value switch
-                {
-                    "Do zrobienia" => "todo",
-                    "W trakcie" => "in_progress",
-                    "Zakończone" => "done",
-                    _ => value
-                };
+                if (string.Equals(value, "Do zrobienia", StringComparison.OrdinalIgnoreCase)) Status = "todo";
+                else if (string.Equals(value, "W trakcie", StringComparison.OrdinalIgnoreCase)) Status = "In progress";
+                else if (string.Equals(value, "Zakończone", StringComparison.OrdinalIgnoreCase)) Status = "done";
+                else Status = value;
             }
         }
     }
