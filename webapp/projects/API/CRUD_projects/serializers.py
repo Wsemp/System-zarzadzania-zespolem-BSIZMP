491f72035e7adea5981973371265f38a44d57522
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-20 20:44:16 +0200

    projects serializers update

--- webapp/projects/API/CRUD_projects/serializers.py
+++ webapp/projects/API/CRUD_projects/serializers.py
@@ -1,14 +1,22 @@
+from django.contrib.auth.models import User
 from rest_framework import serializers
 from ...models import Project
 
+class ProjectMemberActionSerializer(serializers.Serializer):
+    user_id = serializers.IntegerField()
+
+class UserShortSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
-    url = serializers.HyperlinkedIdentityField(
-        view_name='project-detail'
-    )
+    url = serializers.HyperlinkedIdentityField(view_name='project-detail')
+    members = UserShortSerializer(many=True, read_only=True)
 
     class Meta:
         model = Project
-        fields = ['url', 'id', 'name', 'description']
+        fields = ['url', 'id', 'name', 'description', 'members']
         read_only_fields = ['owner']
 
     def create(self, validated_data):
