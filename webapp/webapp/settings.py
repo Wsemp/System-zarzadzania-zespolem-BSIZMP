bd54013221613d145575779ad902fd338191dd66
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-22 20:28:36 +0200

    email settings - added

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -126,6 +126,16 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = "wiktor.semp@studenci.collegiumwitelona.pl"
+EMAIL_HOST_PASSWORD = "ythd bkzw ditl ogpp"
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 LANGUAGE_CODE = 'en-us'
 
