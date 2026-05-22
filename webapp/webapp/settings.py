01f7e0211c977ba4fa0ba47d9a07c6c91e982a5b
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-22 23:45:26 +0200

    settings update

--- webapp/webapp/settings.py
+++ webapp/webapp/settings.py
@@ -128,10 +128,10 @@ AUTH_PASSWORD_VALIDATORS = [
 
 FRONTEND_URL = "https://system-zarzadzania-zespolem-bsizmp.onrender.com"
 
-EMAIL_BACKEND=os.getenv("EMAIL_BACKEND")
-EMAIL_HOST=os.getenv("EMAIL_HOST")
-EMAIL_PORT=os.getenv("EMAIL_PORT")
-EMAIL_USE_TLS=os.getenv("EMAIL_USE_TLS")
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST = "smtp.gmail.com"
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
 
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
