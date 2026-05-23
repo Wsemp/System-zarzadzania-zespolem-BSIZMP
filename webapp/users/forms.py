5497c2b67c7ab7214861a6e2c2c83c28ab7aa129
Wsemp <wiktor.semp@studenci.collegiumwitelona.pl>
2026-05-23 09:42:28 +0200

    security test added

--- webapp/users/forms.py
+++ webapp/users/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from django.contrib.auth import get_user_model
+from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
@@ -20,8 +22,19 @@ class UserCreateForm(forms.ModelForm):
         if email == "":
             return email
         if User.objects.filter(email=email).exists():
-            raise forms.ValidationError("Email już istnieje")
+            raise forms.ValidationError("Email already exist")
         return email
+
+    def clean_password(self):
+        password = self.cleaned_data.get("password")
+
+        if password:
+            try:
+                validate_password(password)
+            except ValidationError as e:
+                raise forms.ValidationError(e.messages)
+
+        return password
 
     def clean(self):
         cleaned_data = super().clean()
@@ -36,7 +49,7 @@ class UserCreateForm(forms.ModelForm):
 class UserUpdateForm(forms.ModelForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'is_staff']
+        fields = ['username', 'email', 'is_staff', 'is_active']
         help_texts = {
             "username": "",
         }
